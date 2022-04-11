ebd39dcaf77a47dbeb248197c0b6e1f1ddb99703
Sofa <wsr11@tk34.mskobr.ru>
2022-04-11 16:46:45 +0600

    задание 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,9 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+
         {
+            //Задание 1
             Console.WriteLine("Hello World!");
         }
     }
