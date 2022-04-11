164218348b619b46ccee6dfd344bc9fc63e4d62f
Sofa <wsr11@tk34.mskobr.ru>
2022-04-11 16:52:24 +0600

    задание 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp1
             int a = 1;
             //Задание 2
             double b = 4;
+            // Задание 3
+            var c = 10;
+            //Console.WriteLine
+
 
 
         }
